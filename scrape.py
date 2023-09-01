39e6bf4e059f1312a0f5bb076d097defbc4e51ca
Hoyoung Lee <sohostory@gmail.com>
2023-09-01 14:23:37 -0500

    hacker news scraper part 1

--- scrape.py
+++ scrape.py
@@ -6,4 +6,19 @@ res = requests.get('https://news.ycombinator.com/news')
 soup = BeautifulSoup(res.text, 'html.parser')
 links = soup.select('.titleline > a')
 scores = soup.select('.score')
-print(scores)
+
+
+def create_custom_hn(links, votes):
+    hn = []
+    for idx, item in enumerate(links):
+        title = links[idx].getText()
+        href = links[idx].get('href')
+        point = int(votes[idx].getText().replace(' points', '')
+        hn.append({
+            'title': title,
+            'link': href
+        })
+    return hn
+
+
+print(create_custom_hn(links, scores))
